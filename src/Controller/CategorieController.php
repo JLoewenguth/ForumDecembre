0f76e517e6afbbcc30fc5ae09f2b31c219d9d428
JLoewenguth <jrm.lguth@gmail.com>
2022-12-15 11:53:55 +0100

    sujets dans une catégorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Sujet;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategorieController extends AbstractController
 {
     #[Route('/categorie', name: 'app_categorie')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $sujets =
+        $doctrine->getRepository(Sujet::class)->findBy(['categorie_id' => '1'],[]);
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'sujets'=>$sujets
         ]);
     }
 }
--- src/Controller/SujetController.php
+++ src/Controller/SujetController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Sujet;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SujetController extends AbstractController
 {
     #[Route('/sujet', name: 'app_sujet')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $sujets = $doctrine->getRepository(Sujet::class)->findAll();
         return $this->render('sujet/index.html.twig', [
-            'controller_name' => 'SujetController',
+            'sujets'=> $sujets
         ]);
     }
 }
